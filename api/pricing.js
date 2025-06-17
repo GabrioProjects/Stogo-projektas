d22f9a7f0ba6563385cac43b6db9dbad564ad999
GabrioProjects <215856464+GabrioProjects@users.noreply.github.com>
2025-06-17 15:04:39 +0300

    Delete api/pricing.js

--- api/pricing.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    markup: parseFloat(process.env.MARKUP),
-    pitchMultipliers: JSON.parse(process.env.PITCH_MULTIPLIERS),
-    materials: JSON.parse(process.env.MATERIALS),
-    labor: JSON.parse(process.env.LABOR),
-    complexity: JSON.parse(process.env.COMPLEXITY),
-  });
-}
